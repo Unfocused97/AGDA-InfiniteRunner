35124047704e6fd677cd37a7ec9ba3cd45d9a755
cheeshee <amyliu1.618@gmail.com>
2019-01-17 19:24:11 -0800

    coin collecting added
    coin script added to temp coin object and score script added to player.
    I hope this works

---
+++ Game Controller 2/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.Rotate(0,0, 90 * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Ball")
+        {
+            other.GetComponent<ScoreScript>().points++;
+            // Coin gets removed
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Game Controller 2/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    public int points = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        GUI.contentColor = Color.black;
+        GUI.Label(new Rect(10, 10, 100, 20), "Score: " + points);
+    }
+}

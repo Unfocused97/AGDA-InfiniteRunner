c9f7b77e173da1f0764b01936e9d9384deafe5dd
cheeshee <amyliu1.618@gmail.com>
2019-02-23 14:24:00 -0800

    some sort of popup text added
    not exactly what we want

--- Game Controller 2/Assets/Scripts/CoinScript.cs
+++ Game Controller 2/Assets/Scripts/CoinScript.cs
@@ -6,11 +6,13 @@ public class CoinScript : MonoBehaviour
 {
     public string ingredient;
     public int weight;
+    public GameObject text;
+    private Vector3 textPos = new Vector3(1, 1, 0);
 
     // Use this for initialization
     void Start()
     {
-
+        text.GetComponent<TextMesh>().text = this.ingredient + " picked up!!";
     }
 
     // Update is called once per frame
@@ -23,13 +25,18 @@ public class CoinScript : MonoBehaviour
     {
         if (other.name == "Ball")
         {
+            // pop up text
+            text.GetComponent<TextMesh>().text = this.ingredient + " picked up!!";
+            GameObject yes = Instantiate(text, new Vector3(other.transform.position.x, other.transform.position.y + 1, other.transform.position.z), new Quaternion(0, 0, 0, 0));
             // need to do something about adjusting this speed
             other.GetComponent<PlayerController>().Speed -= 0.3f;
             AddToCollected(other);
+
             // Coin gets removed
             Destroy(gameObject);
         }
     }
+    
 
     private void AddToCollected(Collider other)
     {

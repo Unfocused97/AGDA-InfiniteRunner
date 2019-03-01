82989c799bcec0ecfe7b23110630701ab25af667
cheeshee <amyliu1.618@gmail.com>
2019-02-28 19:58:40 -0800

    all recipes and ingredients added

--- Game Controller 2/Assets/Scripts/CoinScript.cs
+++ Game Controller 2/Assets/Scripts/CoinScript.cs
@@ -26,8 +26,10 @@ public class CoinScript : MonoBehaviour
         if (other.name == "Ball")
         {
             // pop up text
+            /*
             text.GetComponent<TextMesh>().text = this.ingredient + " picked up!!";
             GameObject yes = Instantiate(text, new Vector3(other.transform.position.x, other.transform.position.y + 1, other.transform.position.z), new Quaternion(0, 0, 0, 0));
+            */
             // need to do something about adjusting this speed
             other.GetComponent<PlayerController>().Speed -= 0.3f;
             AddToCollected(other);
--- Game Controller 2/Assets/Scripts/ScoreScript.cs
+++ Game Controller 2/Assets/Scripts/ScoreScript.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class ScoreScript : MonoBehaviour
 {
     public int points = 0;
+    public string message;
 
 
     // Use this for initialization
     void Start()
     {
-
+        message = "Score: ";
     }
 
     // Update is called once per frame
@@ -22,6 +23,6 @@ public class ScoreScript : MonoBehaviour
     private void OnGUI()
     {
         GUI.contentColor = Color.black;
-        GUI.Label(new Rect(10, 10, 100, 20), "Score: " + points);
+        GUI.Label(new Rect(10, 10, 100, 20), message + points);
     }
 }
